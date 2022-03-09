c4fec1a1873b044e42805949a6718dcce34e1c2e
Samuel Robbins <83656803+SRobbins-13@users.noreply.github.com>
2022-03-08 19:36:07 -0500

    Update recipe_dashboard_app.py

--- recipe_dashboard_app.py
+++ recipe_dashboard_app.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 #connect to cloud Mongodb server
 database_name = 'RecipeData'
-client = MongoClient("mongodb://srobbins13:yFgUZTu1s3RLqo6c@recipecluster.qtpe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://srobbins13:yFgUZTu1s3RLqo6c@recipecluster.qtpe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client[database_name]
 
 # create list of recipe titles to pull random recipe from
